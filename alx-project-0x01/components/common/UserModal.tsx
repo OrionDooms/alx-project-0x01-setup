3b7c7715a7cd58f62238358254c7851975a94672
Orion <Orion45@dd.gmail.com>
2025-07-19 21:53:49 +0200

    Implement the User Modal

---
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -0,0 +1,131 @@
+import { UserData, UserModalProps } from "@/interfaces";
+import React, { useState } from "react";
+
+
+
+const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
+	const [user, setUser] = useState<UserData>({
+		id: 0,
+		name: "",
+		username: "",
+		email: "",
+		address: {
+			street: "",
+			suite: "",
+			city: "",
+			zipcode: "",
+			geo: {
+				lat: "",
+				lng: ""
+			}
+		},
+		phone: "",
+		website: "",
+		company: {
+			name: "",
+			catchPhrase: "",
+			bs: ""
+		}
+	})
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const { name, value } = e.target;
+	const[group, key] = name.split(".");
+
+	if (key) {
+		setUser((prev) => ({
+		...prev,
+		[group]: {
+		...prev[group as keyof UserData],
+		[key]: value,
+		},
+		}));
+	} else {
+		setUser((prev) => ({ ...prev, [name]: value}));
+	}
+};
+
+const handleSubmit = (e: React.FormEvent) => {
+	e.preventDefault();
+	onSubmit(user);
+	onClose();
+};
+
+return (
+	<div>
+	<div>
+	<h2>Add New User</h2>
+	<form>
+		<input 
+		type="text"
+		name="name"
+		value={user.name}
+		onChange={handleChange}
+		placeholder="Full Name"
+		className="w-full border rounded px-4 py-2"
+		/>
+		<input 
+		type="text"
+		name="username"
+		value={user.username}
+		onChange={handleChange}
+		placeholder="Username"
+		className="w-full border rounded px-4 py-2"/>
+		<input 
+		type="email"
+		name="email"
+		value={user.email}
+		onChange={handleChange}
+		placeholder="Email"
+		className="w-full border rounded px-4 py-2"/>
+
+		<input 
+		type="text"
+		name="address.city"
+		value={user.address.city}
+		onChange={handleChange}
+		placeholder="City"
+		className="w-full border rounded px-4 py-2" />
+
+		<input 
+		type="text"
+		name="phone"
+		value={user.phone}
+		onChange={handleChange}
+		placeholder="Phone"
+		className="w-full border rounded px-4 py-2" />
+
+		<input 
+		type="text"
+		name="website"
+		value={user.website}
+		onChange={handleChange}
+		placeholder="Website"
+		className="w-full border rounded px-4 py-2" />
+		
+		<input 
+		type="text"
+		name="company.name"
+		value={user.company.name}
+		onChange={handleChange}
+		placeholder="Company Name"
+		className="w-full border rounded px-4 py-2" />
+
+		<div>
+			<button
+			type="button"
+			onClick={onClose}
+			className="bg-gray-300 px-4 py-2 rounded mr-2"
+			>
+				Cancel
+			</button>
+			<button
+			type="submit"
+			className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Add User</button>
+			</div>
+			</form>
+	</div>
+	</div>
+);
+};
+export default UserModal;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,29 +1,47 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
 import { UserProps } from "@/interfaces";
+import { useState } from "react";
 
 interface UserPageProp {
 	posts: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => {
-	return (
-		<div className="flex flex-col h-screen">
-		<Header />
-		<main className="overflow-y-auto p-5">
-		<div className="flex items-center justify-between mb-5">
-		<h1 className="font-semibold text-2xl">User Directory</h1>
-		<button className=" px-4 py-2 rounded-full bg-blue-300 text-while hover:bg-blue-800">Add user</button>
-		</div>
-		
-		<div className="md:grid-cols-2 lg:grid-cols-3 grid grid-cols-1 gap-4">
-		{users.map((user: UserProps, index: number) => (
-			<UserCard key={index} {...user} />
-		))}
-		</div>
-		</main>
-		</div>
-	);
+const Users: React.FC<UserPageProp> = ({ posts }) => {
+	const [users, setUsers] = useState<UserProps[]>(posts);
+	const [showModal, setShowModal] = useState(false);
+
+	const handleAddUser = (newUser: UserProps) => {
+		setUsers((prevUsers) => [...prevUsers, newUser]);
+};
+
+return (
+	<div className="flex flex-col h-screen">
+	<Header />
+	<main className="overflow-y-auto p-5">
+	<div className="flex items-center justify-between mb-5">
+	<h1 className="font-semibold text-2xl">User Directory</h1>
+	
+	<button className=" px-4 py-2 rounded-full bg-blue-300 text-while hover:bg-blue-800" onClick={() => setShowModal(true)}>
+	Add user
+	</button>
+	</div>
+
+	<div className="md:grid-cols-2 lg:grid-cols-3 grid grid-cols-1 gap-4">
+	{users.map((user: UserProps, index: number) => (
+		<UserCard key={index} {...user} />
+	))}
+	</div>
+
+	{showModal && (
+        <UserModal onClose={() => setShowModal(false)}
+        onSubmit={handleAddUser}
+        />
+        )}
+	</main>
+	</div>
+);
 };
 
 export async function getStaticProps() {
